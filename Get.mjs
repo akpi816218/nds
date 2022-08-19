6bebfa0c6cc3af454faf7a047bbfb3e22276592a
Akhil <816218@seq.org>
2022-08-19 13:50:06 -0700

    save

--- Get.mjs
+++ Get.mjs
@@ -2,8 +2,12 @@ import { readFileSync } from 'fs';
 import { Data } from './Data.mjs';
 
 const Get = (key) => {
-	// return JSON.parse(readFileSync(".ndsf", { encoding: "utf-8" }));
-	return new Data(JSON.parse(readFileSync(".ndsf", { encoding: "utf-8" })));
+	let allData = [], json = JSON.parse(readFileSync(".ndsf", { encoding: "utf-8" }));
+	for (let i in json) {
+		allData.push(json[i])
+	}
+	// return new Data(jsondata);
+	return allData;
 }
 
 export { Get };
