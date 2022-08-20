fab4adc7ae4a268d05906dfaa56bd0a94cfa630d
Akhil <816218@seq.org>
2022-08-19 18:54:30 -0700

    save

--- Get.mjs
+++ Get.mjs
@@ -1,13 +1,31 @@
 import { readFileSync } from 'fs';
 import { Data } from './Data.mjs';
 
-const Get = (key) => {
-	let allJson = [], allData = [], json = JSON.parse(readFileSync(".ndsf", { encoding: "utf-8" }));
+const Get = (regexp, case_insensitive = false) => {
+	let allJson = [],
+		allData = [],
+		allHits = [],
+		json = JSON.parse(readFileSync('.ndsf', { encoding: 'utf-8' }));
+
 	for (let i in json) {
-		allJson.push(json[i])
+		allJson.push(json[i]);
 	}
-	// return new Data(jsondata);
+
+	allJson.sort((a, b) => {
+		a = a.key.toLowerCase();
+		b = b.key.toLowerCase();
+		return a < b ? -1 : a > b ? 1 : 0;
+	});
+
+	allHits = allJson.filter((value) => {
+		return regexp.test(value.key) ? true : false;
+	});
+
+	allHits.forEach((value) => {
+		allData.push(new Data(value));
+	});
+
 	return allData;
-}
+};
 
 export { Get };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { Get } from './Get.mjs';
 // import { Set } from "./Set.mjs";
 
-console.log(Get());
+console.log(Get(/(arli|viwo)/i));
